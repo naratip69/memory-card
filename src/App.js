5f4481807a730865ffec3682fb78a6e7d503e78d
Naratip <naratip.18257@gmail.com>
2023-07-14 15:42:45 +0700

    create onclick function

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Card from "./component/Card";
 function App() {
   return (
     <div className="App">
-      <Card />
+      <Card name="TEST" />
     </div>
   );
 }
--- src/component/Card.js
+++ src/component/Card.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from "react";
 
 export default function Card(props) {
-  const { name, src, id } = props;
+  const { name, src, id, isGameOver, setGameOver, score, setScore } = props;
   const [isClick, setClick] = useState(false);
 
+  onclick = (e) => {
+    if (!isClick) {
+      setClick(true);
+      setScore(score + 1);
+    } else {
+      setGameOver(true);
+    }
+  };
+
   return (
-    <div className="card">
+    <div className="card" onClick={onclick}>
       <img src={src} />
       <h5>{name}</h5>
     </div>
